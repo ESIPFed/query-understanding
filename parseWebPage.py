f6dcd85e427b9de1ac2c9757fc2b748978ad1a03
justgo129 <justgo129@users.noreply.github.com>
2018-05-10 15:22:12 -0400

    added function

--- parseWebPage.py
+++ parseWebPage.py
@@ -6,7 +6,13 @@
 https://stackoverflow.com/questions/12965203/how-to-get-json-from-webpage-into-python-script
 '''
 
-import urllib.request, json 
-with urllib.request.urlopen("http://199.26.254.151:8080/mudrod-service/SearchVocab?concept=sst") as url:
-    data = json.loads(url.read().decode())
-    print(json.dumps(data, indent=4))
+import urllib.request, json
+
+myUrl = "http://199.26.254.151:8080/mudrod-service/SearchVocab?concept=sst"
+
+def parse(myUrl):
+    with urllib.request.urlopen(myUrl) as url:
+        data = json.loads(url.read().decode())
+        print(json.dumps(data, indent=4))
+        
+parse(myUrl)
